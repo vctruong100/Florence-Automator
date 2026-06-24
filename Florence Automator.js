2ba2f03d4a84dbeb2dc384c64d43ae5480e96a41
vctruong100 <vctruong100@gmail.com>
2026-06-24 08:50:08 -0700

    Updated version to 2.3.9

--- Florence Automator.js
+++ Florence Automator.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name Florence Automator
 // @namespace vinh.activity.plan.state
-// @version 2.3.8
+// @version 2.3.9
 // @description
 // @match https://us.v2.researchbinders.com/*
 // @updateURL    https://raw.githubusercontent.com/vctruong100/Florence-Automator/main/Florence%20Automator.js
